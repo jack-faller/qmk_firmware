d3e8a205b043c60652716b2dff36c5ae78a6562a
Jack Faller <jack.t.faller@gmail.com>
2025-08-26 14:25:24 +0100

    Makes flipping script directory independent

--- keyboards/cantor/keymaps/make_flipped.sh
+++ keyboards/cantor/keymaps/make_flipped.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -ev
+FLIPPER=$(dirname $(realpath "$0"))/json_flipper
+LOCATION=$(realpath "$1")
+KEYMAP=$(basename "$LOCATION")
+cd $(dirname "$LOCATION")
 # You can plug the USB into either half if you flash the right half with a map
 # that has all the keys mirrored along the vertical axis.
-KEYMAP=$(basename "$1")
-rm -rf "$1"_flipped
-cp -r "$1" "$1"_flipped
-rm "$1"_flipped/"$KEYMAP".json
-$(dirname "$0")/json_flipper "$1"/"$KEYMAP".json > "$1"_flipped/"$KEYMAP"_flipped.json
+rm -rf "$LOCATION"_flipped
+cp -r "$LOCATION" "$LOCATION"_flipped
+rm "$LOCATION"_flipped/"$KEYMAP".json
+"$FLIPPER" "$LOCATION"/"$KEYMAP".json > "$LOCATION"_flipped/"$KEYMAP"_flipped.json
